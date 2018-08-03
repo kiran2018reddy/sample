f5512ca83178978a247ecc2dfe8758932ee50073
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-03 16:35:42 -0700

    " -> 0 of 4 passed. pylint: 10.00/10 "

--- sample-assignment/m5/p1/perfect_cube.py
+++ sample-assignment/m5/p1/perfect_cube.py
@@ -7,10 +7,10 @@ def main():
         if guess_1**3 >= abs(y_1):
             break
     if guess_1**3 != abs(y_1):
-        print (y_1, 'is not a perfect cube')
+        print(y_1, 'is not a perfect cube')
     else:
         if y_1 < 0:
             guess_1 = -guess_1
-    print(str(y_1)+ 'is' + 'a perfect cube')
+    print(str(y_1) + 'is' + 'a perfect cube')
 if __name__ == "__main__":
     main()
